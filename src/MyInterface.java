f1e3c97a8462a8b8e64cd160e6c2573d6bef7b39
Yasminebettaieb <71673006+Yasminebettaieb@users.noreply.github.com>
2021-12-06 19:42:50 +0000

    Update MyInterface.java

--- src/MyInterface.java
+++ src/MyInterface.java
@@ -2,5 +2,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface MyInterface extends Remote {
-
+    char MinChar(String ch) throws RemoteException;
+    StringBuffer CaseChanger(String ch) throws RemoteException;
+    String Reverse(String ch) throws RemoteException;
 }
+
